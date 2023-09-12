9c1f06412a9592e1e691fef73e515b53d83a90a5
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-09-12 10:11:39 -0400

    palindrome lab done

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  if (word.length === 0 || word.length === 1) {
+    return true;
+  } else if (word[0] === word[word.length - 1]) {
+    const inner = word.slice(1, -1);
+    return isPalindrome(inner);
+  } else {
+    return false;
+  }
 }
 
 /* 
-  Add your pseudocode here
+  pseudocode
+  RACECAR
+  base case: if length is zero or one
+    return true
+  if first element equals last element
+    slice off first and last char
+    pass in sliced string recursively
+  else return false
 */
 
 /*
-  Add written explanation of your solution here
+  The code should compare the first and last letters, and if they're equal,
+  continue working towards the middle comparing characters. I can do this by
+  passing in a shorter string recursively whenever the first and last chars are equal,
+  until I get to a base case of length 0 or 1. If at any point the first and last chars are not equal,
+  the function will return false;
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  /* Test cases */
+  console.log(`expect ${isPalindrome("kayak")} to be true`);
+  console.log(`expect ${isPalindrome("bobbob")} to be true`);
+  console.log(`expect ${isPalindrome("rater")} to be false`);
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
